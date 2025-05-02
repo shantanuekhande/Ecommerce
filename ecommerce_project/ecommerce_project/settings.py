7c1f4ba8973732f5f4caafd1f39185e8c2b988ba
shantanu ekhande <shantanuekhande19@gmail.com>
2025-05-02 12:47:12 +0530

    configured AWS RDS

--- ecommerce_project/ecommerce_project/settings.py
+++ ecommerce_project/ecommerce_project/settings.py
@@ -76,14 +76,26 @@ WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'product',
+        'USER' :'admin',
+        'PASSWORD' :'Admin123456789',
+        'HOST' : 'database-1.c3w0y8sg8y82.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306'
+
+
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
